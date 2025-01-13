dcc7dc747a5ae942c8c220547dd089b0535baf4d
Zombie1995 <egorkerber1995@mail.ru>
2025-01-13 22:27:27 +0700

    tg link updated

--- src/widgets/default-layout/ui/telegram/ui/index.tsx
+++ src/widgets/default-layout/ui/telegram/ui/index.tsx
@@ -2,7 +2,7 @@ import telegram from "shared/assets/icons/telegram.svg";
 
 export const Telegram = () => {
   return (
-    <a href="https://t.me/abet_kvn_bot">
+    <a href="https://t.me/abet_kvn">
       <button className="relative size-[64px] bg-white rounded-full">
         <span className="animate-ping absolute top-[12.5%] left-[12.5%] inline-flex h-[75%] w-[75%] rounded-full bg-white opacity-75" />
         <img
